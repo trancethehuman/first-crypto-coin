c99b26fc6e40febe68bfb54b78a6973463018292
Hai Nghiem <hainghiem375@gmail.com>
2019-10-10 13:45:19 -0230

    Making changes
    changes changes changes

--- main.js
+++ main.js
@@ -1,17 +1,38 @@
-class Block{
-    constructor(index, timestamp, data, previousHash=''){
-        //index: each block has an index in the blockchain, tells us where the block sits
-        //timestamp: when the block was created
-        //data: details of the transaction are stored here
-        //previousHash: Hash values of the block before
-        this.index = index;
-        this.timestamp = timestamp;
-        this.data = data;
-        this.previousHash = previousHash;
-        this.hash = '';
-    }
+const { Blockchain, Transaction } = require('./blockchain');
+const EC = require('elliptic').ec;
+const ec = new EC('secp256k1');
 
-    calculateHash() {
-        //Takes the properties of a block, run it through a Hash creator.
-    }
-}
+// Your private key goes here
+const myKey = ec.keyFromPrivate('7c4c45907dec40c91bab3480c39032e90049f1a44f3e18c3e07c23e3273995cf');
+
+// From that we can calculate your public key (which doubles as your wallet address)
+const myWalletAddress = myKey.getPublic('hex');
+
+// Create new instance of Blockchain class
+const haiCoin = new Blockchain();
+
+// Create a transaction & sign it with your key
+const tx1 = new Transaction(myWalletAddress, 'address2', 100);
+tx1.signTransaction(myKey);
+haiCoin.addTransaction(tx1);
+
+// Mine block
+haiCoin.minePendingTransactions(myWalletAddress);
+
+// Create second transaction
+const tx2 = new Transaction(myWalletAddress, 'address1', 50);
+tx2.signTransaction(myKey);
+haiCoin.addTransaction(tx2);
+
+// Mine block
+haiCoin.minePendingTransactions(myWalletAddress);
+
+console.log();
+console.log(`Balance of xavier is ${haiCoin.getBalanceOfAddress(myWalletAddress)}`);
+
+// Uncomment this line if you want to test tampering with the chain
+// haiCoin.chain[1].transactions[0].amount = 10;
+
+// Check if the chain is valid
+console.log();
+console.log('Blockchain valid?', haiCoin.isChainValid() ? 'Yes' : 'No');
